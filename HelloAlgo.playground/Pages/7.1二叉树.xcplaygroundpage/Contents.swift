79e5cc74a9b6bfb5be278c34370b01eadbf946b7
Janlor <JanlorLee@iCloud.com>
2023-12-24 17:39:30 +0800

    二叉树
        edit 3e15840 二叉树

---
+++ HelloAlgo.playground/Pages/7.1二叉树.xcplaygroundpage/Contents.swift
@@ -0,0 +1,40 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/// 二叉树节点类
+class TreeNode {
+    var val: Int // 节点值
+    var left: TreeNode? // 左子节点引用
+    var right: TreeNode? // 右子节点引用
+    
+    init(x: Int) {
+        self.val = x
+    }
+}
+
+/* 1. 初始化二叉树 */
+
+// 初始化节点
+let n1 = TreeNode(x: 1)
+let n2 = TreeNode(x: 2)
+let n3 = TreeNode(x: 3)
+let n4 = TreeNode(x: 4)
+let n5 = TreeNode(x: 5)
+
+// 构建节点之间的引用（指针）
+n1.left = n2
+n1.right = n3
+n2.left = n4
+n2.right = n5
+
+/* 2. 插入与删除节点 */
+
+// 在 n1 -> n2 中间插入节点 P
+let P = TreeNode(x: 0)
+n1.left = P
+P.left = n2
+// 删除节点
+n1.left = n2
+
+//: [Next](@next)
