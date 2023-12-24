734401b38c6cb79712559b5e8831459dfaf22b26
Janlor <JanlorLee@iCloud.com>
2023-12-24 16:52:39 +0800

    Swift 内置 hashValue
        edit 7dffdfb Swift 内置 hashValue
        edit 3e15840 二叉树
        edit 999da46 二叉树遍历

--- HelloAlgo.playground/Pages/6.3哈希算法.xcplaygroundpage/Contents.swift
+++ HelloAlgo.playground/Pages/6.3哈希算法.xcplaygroundpage/Contents.swift
@@ -56,4 +56,36 @@ func rotHash(key: String) -> Int {
     return hash
 }
 
+/* 
+ Swift 内置哈希值 每次编译结果都不同。
+ 哈希值在不同的运行时环境、Swift版本或编译器设置下会发生变化。
+ Swift的哈希算法会受到底层平台、编译器版本和其他因素的影响，
+ 因此在不同的环境下，同一段代码会产生不同的哈希值。
+ */
+
+let num = 3
+let hashNum = num.hashValue
+// -3,320,462,800,473,418,431
+// -6,927,012,715,119,280,882
+
+let bol = true
+let hashBol = bol.hashValue
+// 3,324,483,666,200,687,309
+// 8,505,234,286,599,632,123
+
+let dec = 3.14159
+let hashDec = dec.hashValue
+// 8,514,113,951,134,313,513
+// 4,486,115,913,318,275,717
+
+let str = "Hello 算法"
+let hashStr = str.hashValue
+// -8,440,251,058,690,080,704
+// -3,777,008,248,773,346,209
+
+let arr = [AnyHashable(12836), AnyHashable("小哈")]
+let hashTup = arr.hashValue
+// -3,480,153,804,108,085,009
+// -9,142,078,019,926,761,781
+
 //: [Next](@next)
